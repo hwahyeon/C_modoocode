156915d8f804182549c81b8f96d2b4886a372aa5
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-04-03 17:20:58 +0900

    Create 001.c

---
+++ 04/001.c
@@ -0,0 +1,20 @@
+/* 산술 연산 */
+#include <stdio.h>
+int main() {
+  int a, b;
+  a = 10;
+  b = 3;
+  printf("a + b 는 : %d \n", a + b);
+  printf("a - b 는 : %d \n", a - b);
+  printf("a * b 는 : %d \n", a * b);
+  printf("a / b 는 : %d \n", a / b);
+  printf("a %% b 는 : %d \n", a % b);
+  return 0;
+}
+
+// Result
+// a + b 는 : 13 
+// a - b 는 : 7 
+// a * b 는 : 30 
+// a / b 는 : 3 
+// a % b 는 : 1 
