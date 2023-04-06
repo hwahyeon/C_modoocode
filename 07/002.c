8e1208e619cebb7e37b511e1fac46eb464db82e9
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-04-06 17:38:41 +0900

    Create 002.c

---
+++ 07/002.c
@@ -0,0 +1,31 @@
+/* for 문 응용 */
+#include <stdio.h>
+int main() {
+  int i;
+  int subject, score;
+  double sum_score = 0;
+
+  printf("몇 개의 과목 점수를 입력 받을 것인가요?");
+  scanf("%d", &subject);
+
+  printf("\n 각 과목의 점수를 입력해 주세요 \n");
+  for (i = 1; i <= subject; i++) {
+    printf("과목 %d : ", i);
+    scanf("%d", &score);
+    sum_score = sum_score + score;
+  }
+
+  printf("전체 과목의 평균은 : %.2f \n", sum_score / subject);
+
+  return 0;
+}
+
+// Result
+// 몇 개의 과목 점수를 입력 받을 것인가요?4
+
+//  각 과목의 점수를 입력해 주세요 
+// 과목 1 : 100
+// 과목 2 : 99 
+// 과목 3 : 89
+// 과목 4 : 76
+// 전체 과목의 평균은 : 91.00 
