b1183f37a0436aba1d9da02b7ebfc592d5ec4032
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-04-06 18:15:33 +0900

    Create 006.c

---
+++ 07/006.c
@@ -0,0 +1,17 @@
+/* while 문 */
+#include <stdio.h>
+int main() {
+  int i = 1, sum = 0;
+
+  while (i <= 100) {
+    sum += i;
+    i++;
+  }
+
+  printf("1 부터 100 까지의 합 : %d \n", sum);
+
+  return 0;
+}
+
+// Result
+// 1 부터 100 까지의 합 : 5050 
