4a6b56647a74d95698e16c745089cdfa76dbf3a1
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-04-06 17:34:33 +0900

    Update 001.c

--- 07/001.c
+++ 07/001.c
@@ -1,9 +1,14 @@
+/* 1 부터 19 까지의 합*/
 #include <stdio.h>
 int main() {
-  int i;
-  for (i = 0; i < 20; i++) {
-    printf("숫자 : %d \n", i);
+  int i, sum = 0;
+  for (i = 0; i < 20; ++i) {
+    sum = sum + i;
   }
+  printf("1 부터 19 까지의 합 : %d", sum);
 
   return 0;
 }
+
+// Result
+// 1 부터 19 까지의 합 : 190
